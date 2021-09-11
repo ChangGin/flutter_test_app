890a0a2281caaa33130eefe724c7433296560b0e
ChangGin <mukku.engineering.inc@gmail.com>
2021-09-11 19:35:22 +0900

    master

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Test',
+      title: 'Flutter Test Mac',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
